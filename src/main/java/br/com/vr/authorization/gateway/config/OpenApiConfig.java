78f6452d025af6e0fc9740cb294c0d578f7dc8c7
Ednardo Rubens <ednardorubens@hotmail.com>
2023-04-03 01:13:23 -0300

    Create base project

---
+++ src/main/java/br/com/vr/authorization/gateway/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package br.com.vr.authorization.gateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final OpenAPI OPEN_API = new OpenAPI();
+
+    @Bean
+    @Scope("application")
+    public OpenAPI springOpenAPI(final BuildProperties buildProperties) {
+        return OPEN_API.info(new Info()
+            .title(buildProperties.get("title"))
+            .description(buildProperties.get("description"))
+            .version(buildProperties.getVersion())
+        );
+    }
+
+}
